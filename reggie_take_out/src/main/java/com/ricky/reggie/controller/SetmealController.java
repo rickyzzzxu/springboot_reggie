b49dcc935698a6f990d3795a9906e7662db6c10b
rickyzzzxu <rickyzzzz@163.com>
2022-10-11 21:48:27 +0800

    套餐(批量)启用、(批量)删除

--- reggie_take_out/src/main/java/com/ricky/reggie/controller/SetmealController.java
+++ reggie_take_out/src/main/java/com/ricky/reggie/controller/SetmealController.java
@@ -3,7 +3,6 @@ package com.ricky.reggie.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ricky.reggie.common.R;
-import com.ricky.reggie.dto.DishDto;
 import com.ricky.reggie.dto.SetmealDto;
 import com.ricky.reggie.entity.Category;
 import com.ricky.reggie.entity.Setmeal;
@@ -41,32 +40,32 @@ public class SetmealController {
     }
 
     @GetMapping("/page")
-    public R<Page> page(int page, int pageSize, String name){
+    public R<Page> page(int page, int pageSize, String name) {
         //分页构造器对象
-        Page<Setmeal> pageInfo = new Page<>(page,pageSize);
+        Page<Setmeal> pageInfo = new Page<>(page, pageSize);
         Page<SetmealDto> dtoPage = new Page<>();
 
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         //添加查询条件，根据name进行like模糊查询
-        queryWrapper.like(name != null,Setmeal::getName,name);
+        queryWrapper.like(name != null, Setmeal::getName, name);
         //添加排序条件，根据更新时间降序排列
         queryWrapper.orderByDesc(Setmeal::getUpdateTime);
 
-        setmealService.page(pageInfo,queryWrapper);
+        setmealService.page(pageInfo, queryWrapper);
 
         //对象拷贝
-        BeanUtils.copyProperties(pageInfo,dtoPage,"records");
+        BeanUtils.copyProperties(pageInfo, dtoPage, "records");
         List<Setmeal> records = pageInfo.getRecords();
 
         List<SetmealDto> list = records.stream().map((item) -> {
             SetmealDto setmealDto = new SetmealDto();
             //对象拷贝
-            BeanUtils.copyProperties(item,setmealDto);
+            BeanUtils.copyProperties(item, setmealDto);
             //分类id
             Long categoryId = item.getCategoryId();
             //根据分类id查询分类对象
             Category category = categoryService.getById(categoryId);
-            if(category != null){
+            if (category != null) {
                 //分类名称
                 String categoryName = category.getName();
                 setmealDto.setCategoryName(categoryName);
@@ -78,7 +77,23 @@ public class SetmealController {
         return R.success(dtoPage);
     }
 
+    @DeleteMapping
+    public R<String> delete(@RequestParam List<Long> ids) {
 
+        setmealService.removeWithDish(ids);
+
+        return R.success("删除套餐成功");
+
+    }
+
+    @PostMapping("/status/0")
+    public R<String> update(Setmeal setmeal) {
+
+        setmealService.updateById(setmeal);
+
+        return R.success("修改成功");
+
+    }
 
 
 }
--- reggie_take_out/src/main/java/com/ricky/reggie/service/SetmealService.java
+++ reggie_take_out/src/main/java/com/ricky/reggie/service/SetmealService.java
@@ -4,9 +4,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.ricky.reggie.dto.SetmealDto;
 import com.ricky.reggie.entity.Setmeal;
 
+import java.util.List;
+
 public interface SetmealService extends IService<Setmeal> {
 
     void saveWithDish(SetmealDto setmealDto);
 
-
+    void removeWithDish(List<Long> ids);
 }
